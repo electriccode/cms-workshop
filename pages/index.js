9b2216d42e22f3064da8c70b4abcab72a11c6df9
electriccode <pankajphartiyal90@gmail.com>
2022-05-26 19:08:01 +0530

    responsive

--- pages/index.js
+++ pages/index.js
@@ -25,21 +25,23 @@ export default function Home() {
             width="100%"
             alt=""
           />
-          <div className="overline">24/7 COURSE HELP</div>
-          <h1 className="h2">Learn with Chegg</h1>
-          <p>
-            From first day to finals, get homework help, exam prep & writing
-            support—tailored to your courses.
-          </p>
-          <a
-            className="button"
-            href="https://www.chegg.com/auth?redirect=%2Fcspofferinterstitial%2Fib%3FproductType%3DCHEGG_STUDY%26checkoutFlowType%3DSINGLE_ITEM_PURCHASE%26data%3DeyJpdGVtcyI6W3siY2F0YWxvZ0l0ZW1JZCI6IjkxNDYiLCJwcmljaW5nSWQiOiI1MzA1ZTU5YS04Mzk3LTM5OGYtOTY2Mi05NWEyNzA2Njc0M2IiLCJxdWFudGl0eSI6MSwiY2F0YWxvZ0l0ZW1Tb3VyY2UiOiJORVRTVUlURSIsImFubm90YXRpb25zIjp7fX1dLCJob3N0QW5ub3RhdGlvbnMiOnt9LCJzdWNjZXNzVXJsIjoiLyIsImVycm9yVXJsIjoiLyJ9%26originalSource%3DCONTENTFUL&action=signup&type=simplifiedstudy"
-          >
-            Get started
-          </a>
+          <div className="body">
+            <div className="overline">24/7 COURSE HELP</div>
+            <h1 className="h2">Learn with Chegg</h1>
+            <p>
+              From first day to finals, get homework help, exam prep & writing
+              support—tailored to your courses.
+            </p>
+            <a
+              className="button"
+              href="https://www.chegg.com/auth?redirect=%2Fcspofferinterstitial%2Fib%3FproductType%3DCHEGG_STUDY%26checkoutFlowType%3DSINGLE_ITEM_PURCHASE%26data%3DeyJpdGVtcyI6W3siY2F0YWxvZ0l0ZW1JZCI6IjkxNDYiLCJwcmljaW5nSWQiOiI1MzA1ZTU5YS04Mzk3LTM5OGYtOTY2Mi05NWEyNzA2Njc0M2IiLCJxdWFudGl0eSI6MSwiY2F0YWxvZ0l0ZW1Tb3VyY2UiOiJORVRTVUlURSIsImFubm90YXRpb25zIjp7fX1dLCJob3N0QW5ub3RhdGlvbnMiOnt9LCJzdWNjZXNzVXJsIjoiLyIsImVycm9yVXJsIjoiLyJ9%26originalSource%3DCONTENTFUL&action=signup&type=simplifiedstudy"
+            >
+              Get started
+            </a>
+          </div>
         </section>
+        <h2 className="h2">Here for every moment</h2>
         <section className="cards">
-          <h2 className="h2">Here for every moment</h2>
           <div className="card">
             <img
               src="https://assets.chegg.com/image/upload/c_scale,f_auto,q_auto,w_600/site-assets/marketing/landing-pages/Study/tile-hw.png"
@@ -97,7 +99,7 @@ export default function Home() {
             </a>
           </div>
         </section>
-        <section className="pricing">
+        <section className="hero textonly">
           <p className="h6">Prices starting at USD $6.79/mo., cancel anytime</p>
           <a href="https://www.chegg.com/auth?redirect=%2Fcspofferinterstitial%2Fib%3FproductType%3DCHEGG_STUDY%26checkoutFlowType%3DSINGLE_ITEM_PURCHASE%26data%3DeyJpdGVtcyI6W3siY2F0YWxvZ0l0ZW1JZCI6IjkxNDYiLCJwcmljaW5nSWQiOiI5N2I1MjE2Yy0yZGQwLTM4YzAtYjI2MS1mYjViMDFhNjBmZGEiLCJxdWFudGl0eSI6MSwiY2F0YWxvZ0l0ZW1Tb3VyY2UiOiJORVRTVUlURSIsImFubm90YXRpb25zIjp7fX1dLCJob3N0QW5ub3RhdGlvbnMiOnt9LCJzdWNjZXNzVXJsIjoiLyIsImVycm9yVXJsIjoiLyJ9%26originalSource%3DCONTENTFUL&action=signup&type=simplifiedstudy">
             Sign up
@@ -113,13 +115,15 @@ export default function Home() {
             width="100%"
             alt=""
           />
-          <h2 className="h2">Save up to 90% off textbooks3</h2>
-          <p>The smartest way to rent or buy textbooks. Period.</p>
-          <a className="button" href="https://www.chegg.com/books">
-            Find your books
-          </a>
+          <div className="body">
+            <h2 className="h2">Save up to 90% off textbooks3</h2>
+            <p>The smartest way to rent or buy textbooks. Period.</p>
+            <a className="button" href="https://www.chegg.com/books">
+              Find your books
+            </a>
+          </div>
         </section>
-        <section className="hero">
+        <section className="hero textonly">
           <h1 className="h2">Study time, crunch time, anytime</h1>
           <p>
             Explore some of our best study tools & get 24/7 support for your
